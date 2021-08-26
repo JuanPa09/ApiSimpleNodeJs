39b62ad8cd985089616e23ff03fd0117ad58f940
JuanPa09 <juanpablo.ardon99@gmail.com>
2021-08-26 14:45:12 -0600

    Api NodeJs

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 var express = require('express');
-//var mysql = require('mysql');
 const cors = require('cors')
-//var fs = require('fs')
 
-/* Probando hacer HTTPS */
-/*var https = require('https')
-var privateKey = fs.readFileSync('./key.pem','utf8');
-var certificate = fs.readFileSync('./cert.pem')
-var credentials = {key:privateKey, cert: certificate};*/
-/* */
 
 port = 3000;
 
@@ -17,20 +9,23 @@ var app = express();
 //middlewares
 app.use(express.json()); //Para parsear las peticiones entrantes
 app.use(express.urlencoded({extended: true})); //Para que permita anidar informacion en las rutas
-app.use(express.static('public')) //Para permitir devolver imagenes 
+app.use(express.static('public')) //Para permitir devolver imagenes (poner imagenes en la carpeta public)
+//Ejemplo para obtener una imagen -> localhost:3000/nodejs.jpg
 app.use(cors())
 
 /* ############## Ejemplos ################# */
 
+/* Ruta Raiz (localhost:3000) */
 app.get('/',(req,res)=>{
     res.status(200).send(`Se accedio correctamente al puerto ${port}`);
 })
 
+//localhost:3000/hola
 app.get('/hola',(req,res)=>{
     console.log(req.query.id)
     res.send(`Hola Mundo!, el id es ${req.query.id} y el nombre es ${req.query.nombre}`)
 })
-
+//localhost:3000/adios
 app.get("/adios",(req,res)=>{
     id,nombre = req.query
     res.send(`Adios ${nombre} con id ${id}`)
@@ -48,7 +43,7 @@ app.get('/datos',(req,res)=>{
     console.log(req.body)
     res.send('Enviados')
 })
-
+//Desde postman
 app.post('/datos',(req,res)=>{
     console.log(req.body)
     res.send('Enviados')
@@ -67,18 +62,13 @@ app.put('/actualizacion',(req,res)=>{
 })
 
 /* #################### API ################################### */
-
+//Redirecciona a la carpeta usuario.route
 const usuarioRouter = require('./routes/usuario.route')
 app.use("/usuario",usuarioRouter)
-
+//Redirecciona a la carpeta reserva.route
 const reservaRouter = require('./routes/reserva.route')
 app.use('/reserva',reservaRouter)
 
 app.listen(port,function(){
     console.log("Corriendo en el puerto " + port)
 })
-
-
-/* Probando HTTPS */
-/*var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(port);*/
