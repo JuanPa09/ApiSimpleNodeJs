4a3d304eb88616ad1a5db66ca98c39d631102e03
JuanPa09 <juanpablo.ardon99@gmail.com>
2021-08-25 19:35:01 -0600

    Usando protocolo https

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 //var mysql = require('mysql');
 const cors = require('cors')
+var fs = require('fs')
+
+/* Probando hacer HTTPS */
+var https = require('https')
+var privateKey = fs.readFileSync('./key.pem','utf8');
+var certificate = fs.readFileSync('./cert.pem')
+var credentials = {key:privateKey, cert: certificate};
+/* */
 
 port = 3000;
 
@@ -66,6 +74,11 @@ app.use("/usuario",usuarioRouter)
 const reservaRouter = require('./routes/reserva.route')
 app.use('/reserva',reservaRouter)
 
-app.listen(port,function(){
+/*app.listen(port,function(){
     console.log("Corriendo en el puerto " + port)
-})
+})*/
+
+
+/* Probando HTTPS */
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port);
